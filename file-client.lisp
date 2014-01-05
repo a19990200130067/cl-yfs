8eba0b84cb9896730c811d1f5f90061ea70ea5bb
John <john_zh_007@yahoo.com>
2014-01-05 10:14:25 +0800

    give the inode layout, write helper function

--- file-client.lisp
+++ file-client.lisp
@@ -2,6 +2,11 @@
 ;;;; Which includes read/write, and the management of indirect blocks
 ;;;; This file should be loaded after the rpc-clients.lisp file
 
+;;;; The inode block layout
+;;;; +--------------+-------+-------+-------+--------+------+
+;;;; | size (32Bit) | atime | ctime | mtime | refcnt | ptrs |
+;;;; +--------------+-------+-------+-------+--------+------+
+
 (in-package :cl-yfs-clients)
 
 (defclass file-client ()
@@ -27,3 +32,19 @@
 (export 'write-content)
 (export 'get-metadata)
 (export 'put-metadata)
+
+
+;;; ldb is used to find the nth byte of a number
+  
+(defun put-int32-to-string (num str pos)
+  (loop for i from 0 to 3 do
+       (setf (char str (+ pos i)) (code-char (ldb (byte 8 (* i 8)) num))))
+  str)
+
+(defun load-int32-from-string (str pos)
+  (reduce (lambda (x y)
+	    (+ (ash x 8) y))
+	  (map 'list (lambda (index) 
+		       (char-code (char str (+ pos index))))
+	       '(3 2 1 0))))
+
